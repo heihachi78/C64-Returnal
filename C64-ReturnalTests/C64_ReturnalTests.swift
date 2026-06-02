888ff3ecee120a5055dcb995c8b2cb021023b435
Tóth István <tott.isti@gmail.com>
2026-06-02 14:18:25 +0200

    Initial Commit

---
+++ C64-ReturnalTests/C64_ReturnalTests.swift
@@ -0,0 +1,19 @@
+//
+//  C64_ReturnalTests.swift
+//  C64-ReturnalTests
+//
+//  Created by Tóth István on 2026. 06. 02..
+//
+
+import Testing
+@testable import C64_Returnal
+
+struct C64_ReturnalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ C64-ReturnalUITests/C64_ReturnalUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  C64_ReturnalUITestsLaunchTests.swift
+//  C64-ReturnalUITests
+//
+//  Created by Tóth István on 2026. 06. 02..
+//
+
+import XCTest
+
+final class C64_ReturnalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
